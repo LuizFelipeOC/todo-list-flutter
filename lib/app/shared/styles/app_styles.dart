637931cc426f407ec9051cb252880e4a5e4a7d5d
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-07-06 09:45:27 -0400

    feat: :sparkles: add responsable texts styles classs from app

---
+++ lib/app/shared/styles/app_styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:todo_list/app/shared/colors/app_colors.dart';
+
+class AppStyles {
+  static const TextStyle heading = TextStyle(
+    fontWeight: FontWeight.w600,
+    color: AppColors.white,
+    fontFamily: 'Roboto',
+    fontSize: 22,
+  );
+
+  static const TextStyle subHeading = TextStyle(
+    fontWeight: FontWeight.w400,
+    color: AppColors.white,
+    fontFamily: 'Roboto',
+    fontSize: 18,
+  );
+}
