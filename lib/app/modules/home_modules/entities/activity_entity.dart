a07352971e085d11eb8f271c5233a944bfa678c8
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-07-06 15:05:39 -0400

    refactor: :recycle: remove items of obejcto not used in map or render ui

--- lib/app/modules/home_modules/entities/activity_entity.dart
+++ lib/app/modules/home_modules/entities/activity_entity.dart
@@ -5,21 +5,11 @@ class ActivityEntity {
   int? id;
   String? title;
   String? content;
-  String? iniHours;
-  String? finalHours;
-  String? status;
-  String? createAt;
-  String? updateAt;
 
   ActivityEntity({
     this.id,
     this.title,
     this.content,
-    this.iniHours,
-    this.finalHours,
-    this.status,
-    this.createAt,
-    this.updateAt,
   });
 
   Map<String, dynamic> toMap() {
@@ -27,24 +17,14 @@ class ActivityEntity {
       'id': id,
       'title': title,
       'content': content,
-      'iniHours': iniHours,
-      'finalHours': finalHours,
-      'status': status,
-      'createAt': createAt,
-      'updateAt': updateAt,
     };
   }
 
   factory ActivityEntity.fromMap(Map<String, dynamic> map) {
     return ActivityEntity(
-      id: map['id'] as int,
-      title: map['title'] as String,
-      content: map['content'] as String,
-      iniHours: map['iniHours'] as String,
-      finalHours: map['finalHours'] as String,
-      status: map['status'] as String,
-      createAt: map['createAt'] as String,
-      updateAt: map['updateAt'] as String,
+      id: map['id'] != null ? map['id'] as int : null,
+      title: map['title'] != null ? map['title'] as String : null,
+      content: map['content'] != null ? map['content'] as String : null,
     );
   }
 
