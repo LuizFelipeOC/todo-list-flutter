b5247e08216430f62688d53db1e0621ded4a877c
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-07-06 13:39:18 -0400

    refactor: :recycle: add new color in app

--- lib/app/shared/colors/app_colors.dart
+++ lib/app/shared/colors/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/cupertino.dart';
 class AppColors {
   static const Color backgroundColor = Color(0xFF10121f);
   static const Color white = Color(0xFFFFFFFF);
+  static const Color green = Color(0xFF69d1a4);
 }
